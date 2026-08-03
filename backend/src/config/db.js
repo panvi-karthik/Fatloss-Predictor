f3f8e274ba0e1e2d1fc0b1919095c7d1ebc2f9c6
PANVI KARTHIK  PACHALLA <135578814+panvi-karthik@users.noreply.github.com>
2026-08-03 20:22:39 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+
+
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'fitpredict_db',
   process.env.DB_USER || 'root',
   process.env.DB_PASSWORD || '',
   {
+
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT || 3306),
     dialect: 'mysql',
@@ -16,6 +19,7 @@ const sequelize = new Sequelize(
       createdAt: 'created_at',
       updatedAt: false
     }
+    
   }
 );
 
